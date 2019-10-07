6c18b7146bec0ec7c091da120ab26e14a1df5891
Jonaszz2301 <55717665+Jonaszz2301@users.noreply.github.com>
2019-10-07 14:43:17 -0400

    Oct 7 2:43PM

--- library.R
+++ library.R
@@ -82,7 +82,7 @@ recode.0S <- function(f) {
 
 
 # convert offense codes to human-readable names
-offense.codes <- read.delim('src/offense-codes.tsv', header=FALSE, col.names=c('code','offense'))
+offense.codes <- read.delim('data_hw3/offense-codes.tsv', header=FALSE, col.names=c('code','offense'))
 offense.codes$offense <- tolower(offense.codes$offense)
 convert.offense.code <- function(codes) {
 	offenses <- offense.codes$offense[as.integer(codes)]
@@ -92,7 +92,7 @@ convert.offense.code <- function(codes) {
 
 
 # combine and standardize top 100 reasons for arrest.
-arrest.offenses <- read.delim('src/arrest.offenses.tsv', header=FALSE, col.names=c('real.offense','nominal.offense'))
+arrest.offenses <- read.delim('data_hw3/arrest.offenses.tsv', header=FALSE, col.names=c('real.offense','nominal.offense'))
 arrest.offenses$real.offense <- trim(arrest.offenses$real.offense)
 arrest.offenses$nominal.offense <- trim(arrest.offenses$nominal.offense)
 convert.arrest.reasons <- function(rawlist) {
